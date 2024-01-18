15bbaccadcbc829c29e2a4a05da056fce932aab7
yusuf habib alfatha <y.habib.alfatha@gmail.com>
2024-01-18 19:44:46 +0800

    Add font family-Poppins

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
     return (
         <div>
-            <h1>this is portfolio</h1>
+            <h1 className="font-Poppins">Test poppins regular</h1>
+            <h1 className="font-Poppins text-6xl font-bold">
+                Test poppins bold
+            </h1>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                Poppins: ['Poppins'],
+            },
+        },
+    },
+    plugins: [],
 }
-
