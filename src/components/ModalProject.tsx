dd1909e17a3bbf84bfc5348e86b6884483de968a
yusufhabibalfatha <yusufhabibalfatha@yusufhabibalfathas-MacBook-Pro.local>
2024-05-12 21:55:19 +0800

    Change project details from tansition to use modal

--- src/components/ModalProject.tsx
+++ src/components/ModalProject.tsx
@@ -1,21 +1,23 @@
 import { MutableRefObject } from "react";
 
-type asdasd: HTMLDialogElement | null;
+interface modalProp {
+  test: MutableRefObject<HTMLDialogElement | null>;
+}
 
-const ModalProject = ({ test }: asdasd) => {
+const ModalProject = ({ test }: modalProp) => {
   return (
     <dialog id="my_modal_1" className="modal modal-bottom" ref={test}>
       <div className="flex flex-col justify-between h-screen modal-box">
         <div className="h-full overflow-y-scroll ">
           <div className="h-screen ">asd</div>
         </div>
-        <div className=" modal-action">
+        <div className="modal-action">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn">Close</button>
-            {/* <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">
+            <button className="absolute top-2 btn btn-sm btn-circle btn-ghost right-2">
               ✕
-            </button> */}
+            </button>
           </form>
         </div>
       </div>
